b34886135c87843e8762f5970227b2510fa7fba8
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-07-29 00:15:27 -0300

    init commit
    se agregan botones para redireccion provisioriamente

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Calculadora.css'
+import Button from './Boton'
+import './Home.css'
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                home
+            <div id="calculadora" className="home__home">
+                <h2 id="home__home">Rolgus Tool-Box</h2>
+                    <div onClick={this.onClick} className="boton__home">
+                <Link to="/Portales">
+                        <Button name="Enlazar Portales" />
+                </Link>
+                    </div>
+                    <div onClick={this.onClick} className="boton__home">
+                <Link to="/RestBlocks">
+                        <Button name="Distancias" />
+                </Link>
+                    </div>
+                    <div onClick={this.onClick} className="boton__home">
+                <Link to="/Circulos">
+                        <Button name="Circulos(en desarrollo)" />
+                </Link>
+                    </div>
             </div>
         )
     }

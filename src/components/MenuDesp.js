c3ce2e7a05b6e2e2fe328c4aa64548c7d440e04a
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-07-29 17:11:02 -0300

    animate menu - add flags to "Circulos"

--- src/components/MenuDesp.js
+++ src/components/MenuDesp.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import './MenuDesp.css'
+import depflags from '../flags.json'
 export class MenuDesp extends Component {
 
     onClick = () => {
-        this.props.setMenu(false)
+        setTimeout(() => {
+
+            this.props.setMenu(false)
+        }, 100)
 
     }
 
     render() {
         return (
-            <div id="menu__responsive-des" >
+            <div id="menu__responsive-des" style={this.props.styles}>
                 <Link to="/"><div className="menu__res-li" onClick={this.onClick}>Home</div></Link>
                 <Link to="/Portales"><div onClick = {this.onClick}className="menu__res-li">Portales</div></Link>
                 <Link to="/RestBlocks"><div onClick = {this.onClick}className="menu__res-li">Distancia</div></Link>
-                <Link to="/Circulos"><div onClick = {this.onClick}className="menu__res-li">Circulos</div></Link>
+                {depflags.Circulos && <Link to="/Circulos"><div onClick={this.onClick} className="menu__res-li">Circulos</div></Link>}
+                <div className="menu__res-li-cuenta">
+                    {/* <Link to="/RestBlocks" ><div onClick={this.onClick} className="menu__res-li ">Cuenta</div></Link> */}
+                </div>
+
             </div>
         )
     }

666cf1a678188a75199119555a749a8941407a60
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-11-08 15:41:37 -0300

    solved noreferrer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './NavBar.css'
-import rolgusIcon from '../images/icon.png'
-
+import "./NavBar.css";
+import rolgusIcon from "../images/icon.png";
 
 class NavBar extends React.Component {
-
-    click = () => {
-        
-        this.props.setMenu(!this.props.menuState)
-            
-    }
-    render() {
-        return (
-            <div id="navbar">
-                <a href="https://discord.gg/NKMzAmT4SD " target="_blank"><img src={rolgusIcon} alt="Rolgus Minecraft Skin Face " /></a>
-                <Link to="/">Rolgus</Link>
-                <i className='bx bx-menu' onClick={this.click}></i>
-                
-            </div>
-        )
-    }
+  click = () => {
+    this.props.setMenu(!this.props.menuState);
+  };
+  render() {
+    return (
+      <div id="navbar">
+        <a
+          href="https://discord.gg/NKMzAmT4SD "
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={rolgusIcon} alt="Rolgus Minecraft Skin Face " />
+        </a>
+        <Link to="/">Rolgus</Link>
+        <i className="bx bx-menu" onClick={this.click}></i>
+      </div>
+    );
+  }
 }
-
 
 export default NavBar;
